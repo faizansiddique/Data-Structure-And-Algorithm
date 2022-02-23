bed081ac5280d3437a66c25b853bad3e39e80b42
Mohammed Faizan Siddique <faizan6881@gmail.com>
2022-02-23 10:23:21 +0530

    Search Algorithm

---
+++ src/searchAlgorithm/LinearSearch.java
@@ -0,0 +1,23 @@
+package searchAlgorithm;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+		System.out.println(linearSearch(intArray, 1));
+		System.out.println(linearSearch(intArray, 35));
+		System.out.println(linearSearch(intArray, 88));
+		System.out.println(linearSearch(intArray, -22));
+	}
+
+	public static int linearSearch(int[] input, int value) {
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
